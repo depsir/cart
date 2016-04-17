dbc5dd325243639485b90ba90e21014b96dbef88
Francesco Sacchi <depsir@gmail.com>
2016-04-17 22:40:19 +0200

    Added catalog and item id

--- item.py
+++ item.py
@@ -1,7 +1,8 @@
 class Item(object):
     """Represent a sellable item of the catalog"""
-    def __init__(self, imported, category, name, price):
+    def __init__(self, id, imported, category, name, price):
         super(Item, self).__init__()
+        self.id = id
         self.imported = imported
         self.category = category
         self.name = name

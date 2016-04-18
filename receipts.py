119440cc68ebde2f8c3395bed24921b581c6a319
Francesco Sacchi <depsir@gmail.com>
2016-04-18 22:45:01 +0200

    Removed tax logic from main and implemented cart

--- receipts.py
+++ receipts.py
@@ -1,5 +1,4 @@
-import math
-
+from cart import Cart
 from item import Item
 
 basic_tax = 0.1
@@ -8,40 +7,23 @@ basic_tax_exclude = ['book', 'food', 'medical']
 import_tax = 0.05
 
 
-def round_to_up05(val):
-    return math.ceil(val*100/5)*5/100
+cart1 = Cart(basic_tax, import_tax, basic_tax_exclude)
+cart1.add_item(Item(False, 'book', 'book', 12.49))
+cart1.add_item(Item(False, 'stuff', 'music CD', 14.99))
+cart1.add_item(Item(False, 'food', 'chocolate bar', 0.85))
+cart1.print_receipt()
+print
 
+cart2 = Cart(basic_tax, import_tax, basic_tax_exclude)
+cart2.add_item(Item(True, 'food', 'box of chocolates', 10.00))
+cart2.add_item(Item(True, 'stuff', 'bottle of perfume', 47.50))
+cart2.print_receipt()
+print
 
-def calc_tax_for_item(item):
-    price = item.price
-    import_tax_amount = 0
-    basic_tax_amount = 0
-
-    if item.imported:
-        import_tax_amount = round_to_up05(price * import_tax)
-
-    if item.category not in basic_tax_exclude:
-        basic_tax_amount = round_to_up05(price * basic_tax)
-    tax_amount = import_tax_amount + basic_tax_amount
-
-    return {'base_price': price,
-            'import_tax_amount': import_tax_amount,
-            'basic_tax_amount': basic_tax_amount,
-            'tax_amount': tax_amount,
-            'price_after_taxes': price + tax_amount
-            }
-
-items = []
-items.append(Item(False, 'book', 'book', 12.49))
-items.append(Item(False, 'stuff', 'music CD', 14.99))
-items.append(Item(False, 'food', 'chocolate bar', 0.85))
-items.append(Item(True, 'food', 'box of chocolates', 10.00))
-items.append(Item(True, 'stuff', 'bottle of perfume', 47.50))
-items.append(Item(True, 'stuff', 'bottle of perfume', 27.99))
-items.append(Item(False, 'stuff', 'bottle of perfume', 18.99))
-items.append(Item(False, 'medical', 'packet of headache pills', 9.75))
-items.append(Item(True, 'food', 'chocolates', 11.25))
-
-for item in items:
-    print item.name
-    print calc_tax_for_item(item)
+cart3 = Cart(basic_tax, import_tax, basic_tax_exclude)
+cart3.add_item(Item(True, 'stuff', 'bottle of perfume', 27.99))
+cart3.add_item(Item(False, 'stuff', 'bottle of perfume', 18.99))
+cart3.add_item(Item(False, 'medical', 'packet of headache pills', 9.75))
+cart3.add_item(Item(True, 'food', 'chocolates', 11.25))
+cart3.print_receipt()
+print

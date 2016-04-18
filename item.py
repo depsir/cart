6b3633820a900cf50f6f358644c6c8d9f3a82c47
Francesco Sacchi <depsir@gmail.com>
2016-04-18 22:38:52 +0200

    Removed catalog and item id to keep things simpler

--- item.py
+++ item.py
@@ -1,8 +1,7 @@
 class Item(object):
     """Represent a sellable item of the catalog"""
-    def __init__(self, id, imported, category, name, price):
+    def __init__(self, imported, category, name, price):
         super(Item, self).__init__()
-        self.id = id
         self.imported = imported
         self.category = category
         self.name = name

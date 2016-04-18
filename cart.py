b0095abd13d52b87184e548b4182a603094ff8f3
Francesco Sacchi <depsir@gmail.com>
2016-04-18 22:43:41 +0200

    Added tax logic inside Cart

--- cart.py
+++ cart.py
@@ -3,16 +3,36 @@ import math
 
 class Cart(object):
     """Simple cart object"""
-    def __init__(self):
+    def __init__(self, basic_tax, import_tax, basic_tax_exclude):
         super(Cart, self).__init__()
 
         self.items = []
 
+        self.basic_tax = basic_tax
+        self.basic_tax_exclude = basic_tax_exclude
+        self.import_tax = import_tax
 
     def apply_taxes(self):
         pass
     def add_item(self, item):
         self.items.append(item)
+
+    def round_to_up05(self, val):
+        return math.ceil(val*100/5)*5/100
+
+    def calc_tax_for_item(self, item):
+        price = item.price
+        import_tax_amount = 0
+        basic_tax_amount = 0
+
+        if item.imported:
+            import_tax_amount = self.round_to_up05(price * self.import_tax)
+
+        if item.category not in self.basic_tax_exclude:
+            basic_tax_amount = self.round_to_up05(price * self.basic_tax)
+        tax_amount = import_tax_amount + basic_tax_amount
+
+        return tax_amount
 
     def print_receipt(self):
         # 1 book: 12.49
@@ -20,7 +40,15 @@ class Cart(object):
         # 1 chocolate bar: 0.85
         # Sales Taxes: 1.50
         # Total: 29.83
-        pass
-
-    def checkout(self):
-        pass
+        receipt_lines = []
+        tax_total = 0
+        total = 0
+        for item in self.items:
+            tax = self.calc_tax_for_item(item)
+            item_total_price = tax + item.price
+            receipt_lines.append('1 %s: %.2f' % (item.name, item_total_price))
+            tax_total += tax
+            total += item_total_price
+        receipt_lines.append('Sales Taxes: %.2f' % tax_total)
+        receipt_lines.append('Total: %.2f' % total )
+        print '\n'.join(receipt_lines)

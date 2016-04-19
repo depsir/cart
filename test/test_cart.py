6f39def11e12aadd73917d2681a453f9eceb7651
Francesco Sacchi <depsir@gmail.com>
2016-04-19 10:25:16 +0200

    Refactored test for example input into one class

--- test/test_cart.py
+++ test/test_cart.py
@@ -12,13 +12,24 @@ class TestWithBasicCart(unittest.TestCase):
 
         return Cart(basic_tax, import_tax, basic_tax_exclude)
 
-class TestCartInput1(TestWithBasicCart):
+
+class TestCartExampleInputs(TestWithBasicCart):
 
     def setUp(self):
-        self.cart = self.get_basic_cart()
-        self.cart.add_item(Item(False, 'book', 'book', 12.49))
-        self.cart.add_item(Item(False, 'stuff', 'music CD', 14.99))
-        self.cart.add_item(Item(False, 'food', 'chocolate bar', 0.85))
+        self.cart1 = self.get_basic_cart()
+        self.cart1.add_item(Item(False, 'book', 'book', 12.49))
+        self.cart1.add_item(Item(False, 'stuff', 'music CD', 14.99))
+        self.cart1.add_item(Item(False, 'food', 'chocolate bar', 0.85))
+
+        self.cart2 = self.get_basic_cart()
+        self.cart2.add_item(Item(True, 'food', 'box of chocolates', 10.00))
+        self.cart2.add_item(Item(True, 'stuff', 'bottle of perfume', 47.50))
+
+        self.cart3 = self.get_basic_cart()
+        self.cart3.add_item(Item(True, 'stuff', 'bottle of perfume', 27.99))
+        self.cart3.add_item(Item(False, 'stuff', 'bottle of perfume', 18.99))
+        self.cart3.add_item(Item(False, 'medical', 'packet of headache pills', 9.75))
+        self.cart3.add_item(Item(True, 'food', 'box of chocolates', 11.25))
 
     def test_output_1(self):
         expected_result = """1 book: 12.49
@@ -26,32 +37,14 @@ class TestCartInput1(TestWithBasicCart):
 1 chocolate bar: 0.85
 Sales Taxes: 1.50
 Total: 29.83"""
-        self.assertEqual(self.cart.build_receipt(), expected_result)
-
-
-class TestCartInput2(TestWithBasicCart):
-
-    def setUp(self):
-        self.cart = self.get_basic_cart()
-        self.cart.add_item(Item(True, 'food', 'box of chocolates', 10.00))
-        self.cart.add_item(Item(True, 'stuff', 'bottle of perfume', 47.50))
+        self.assertEqual(self.cart1.build_receipt(), expected_result)
 
     def test_output_2(self):
         expected_result = """1 imported box of chocolates: 10.50
 1 imported bottle of perfume: 54.65
 Sales Taxes: 7.65
 Total: 65.15"""
-        self.assertEqual(self.cart.build_receipt(), expected_result)
-
-
-class TestCartInput3(TestWithBasicCart):
-
-    def setUp(self):
-        self.cart = self.get_basic_cart()
-        self.cart.add_item(Item(True, 'stuff', 'bottle of perfume', 27.99))
-        self.cart.add_item(Item(False, 'stuff', 'bottle of perfume', 18.99))
-        self.cart.add_item(Item(False, 'medical', 'packet of headache pills', 9.75))
-        self.cart.add_item(Item(True, 'food', 'box of chocolates', 11.25))
+        self.assertEqual(self.cart2.build_receipt(), expected_result)
 
     def test_output_3(self):
         expected_result = """1 imported bottle of perfume: 32.19
@@ -60,7 +53,7 @@ class TestCartInput3(TestWithBasicCart):
 1 imported box of chocolates: 11.85
 Sales Taxes: 6.70
 Total: 74.68"""
-        self.assertEqual(self.cart.build_receipt(), expected_result)
+        self.assertEqual(self.cart3.build_receipt(), expected_result)
 
 
 class TestRound_to_up05(TestWithBasicCart):

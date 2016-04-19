535eb54596317fe38d1bfc9f380ef9adb6e14424
Francesco Sacchi <depsir@gmail.com>
2016-04-19 12:02:29 +0200

    Added item documentation

--- item.py
+++ item.py
@@ -1,5 +1,12 @@
 class Item(object):
-    """Represent a sellable item of the catalog"""
+    """Represent a sellable item of the catalog
+
+    An item is described by:
+    - name: A string that identifies this item
+    - price: the shelf price before taxes
+    - category: a string describing the item category
+    - imported: a boolean which is true if this item is imported
+    """
     def __init__(self, imported, category, name, price):
         super(Item, self).__init__()
         self.imported = imported
